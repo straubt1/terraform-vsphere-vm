5b65106fe28167532ce30f161695b029852237aa
Tom Straub <tstraub@hashicorp.com>
2020-03-25 18:57:33 -0500

    Initial

---
+++ main.tf
@@ -0,0 +1,45 @@
+variable "datacenter_name" {}
+variable "cluster_name" {}
+variable "datastore_name" {}
+variable "network_name" {}
+variable "datacenter_name" {}
+variable "virtual_machine_name" {}
+
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter_name
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.virtual_machine_name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 2
+  memory           = 1024
+  guest_id         = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  wait_for_guest_net_timeout = 0
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
